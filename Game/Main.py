cf113b243313e812a3d0ae8736054d9bcced96a7
Wiktor <s203519@student.pg.edu.pl>
2026-05-15 13:06:05 +0200

    podstawa pygame działa sama w sobie gra ale nie działa coś

--- Game/Main.py
+++ Game/Main.py
@@ -2,9 +2,10 @@
 from Client import GameClient
 from threading import Thread
 import time
+from Gui import GUI
 def start_game():
     client = GameClient(host='127.0.0.1', port=8000)
-
+    game = GUI(client)
     if client.connect():
         
         thread = Thread(target = client.receive_data, args = ())
@@ -20,17 +21,37 @@ def start_game():
 
         #  Główna pętla gry po wystartowaniu
         while client.is_connected:
-            print("halo")
-            if(client.current_players != client.max_players and client.max_players > 0):
-                 print(f"graczy{client.current_players} / {client.max_players}")
-                 print("halo")
-            else:
-                print("halo")
-                print(client.tableCard)
-                print(client.myCards[client.obecna_karta])
-                wybor = str(input("Podaj symbol: "))
-                client.send_move(wybor)
-            pass
+            
+            game.lobby()
+            # if(client.current_players != client.max_players and client.max_players > 0):
+            #      print(f"graczy{client.current_players} / {client.max_players}")
+
+            # else:
+                # Sprawdź czy karty NIE są None ORAZ czy lista nie jest pusta
+            if client.myCards and client.tableCard:
+                game.gameLoop()
+            #         print("\n--- STATUS GRY ---")
+            #         print(f"Karta na stole: {client.tableCard}")
+            #         print(f"Twoja karta:    {client.myCards[client.obecna_karta]}")
+                    
+            #         # Prosta obsługa błędnego indeksu na wszelki wypadek
+            #         try:
+            #             wybor = input("Podaj symbol (lub 'q' aby wyjść): ")
+            #         except EOFError:
+            #             break
+
+            #         if wybor == 'q':
+            #             client.close()
+            #             return
+                    
+            #         client.send_move(wybor)
+            #         # Po wysłaniu ruchu dajemy ułamek sekundy na odświeżenie danych z serwera
+            #         time.sleep(0.1)
+            # else:
+            #         # Jeśli wejdzie w else, ale kart jeszcze nie ma, wypisz komunikat raz
+            #         print("Czekam na rozdanie kart...", end="\r")
+            #         time.sleep(0.5)
+            # pass
 
 if __name__ == "__main__":
     start_game()
